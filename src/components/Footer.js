49ad976882cc3f97d206619ec69891ff6039ed98
John <jjvictorac@gmail.com>
2023-04-27 22:35:46 -0300

    fix: footer padding

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,8 +6,8 @@ import { motion } from "framer-motion";
 
 export default function Footer() {
     return (
-        <footer className="w-full border-t-2 border-solid border-colors-light font-medium text-sm bg-colors-dark text-colors-light md:text-xs">
-            <Layout className="py-8 flex items-center justify-between !bg-colors-dark lg:py-6">
+        <footer className="w-full border-t-[1px] border-solid border-colors-light font-medium text-sm bg-colors-dark text-colors-light md:text-xs sm:text-[10px]">
+            <Layout className="py-8 sm:py-4 flex items-center justify-between !bg-colors-dark lg:py-6">
                 <div className="flex gap-2 md:flex-col md:flex-col-reverse">
                     <span >{new Date().getFullYear()} &copy; Todos os Direitos Reservados</span>
                     <span className={`h-[20px] w-[1px] inline-block  bg-colors-light/75 md:hidden`} />
