be0724f99c3ecb3a9a371b8ea08c3a96ff04804a
John <jjvictorac@gmail.com>
2023-04-27 22:39:06 -0300

    fix: hire-me z-index

--- src/components/HireMe.js
+++ src/components/HireMe.js
@@ -2,7 +2,7 @@ import { CircularText, WhatsappIcon } from "./Icons";
 import Link from "next/link";
 
 export default function HireMe() {
-    return (<div className="fixed left-4 bottom-4 flex items-center justify-center overflow-hidden z-10
+    return (<div className="fixed left-4 bottom-4 flex items-center justify-center overflow-hidden z-10 sm:z-0
         md:right-8 md:left-auto md:top-[-60px] md:absolute md:bottom-auto sm:right-0
     ">
         <div className="w-48 h-auto flex items-center justify-center md:w-24">
