c284afde4480a9b460f270d2799954c82457bb97
John <jjvictorac@gmail.com>
2023-04-27 23:17:07 -0300

    fix: verify is First Load to improve cls

--- src/components/TransitionEffect.js
+++ src/components/TransitionEffect.js
@@ -1,24 +1,37 @@
 import { motion } from 'framer-motion';
+import { useEffect, useState } from 'react';
 
 export default function TransitionEffect() {
-    return (<>
-        <motion.div className='fixed top-0 bottom-0 right-full w-screen h-screen z-40 bg-colors-primary'
-            initial={{ x: "100%", width: "100%" }}
-            animate={{ x: "0%", width: "0%" }}
-            exit={{ x: ["0%", "100%"], width: ["0%", "100%"] }}
-            transition={{ duration: 0.8, ease: "easeInOut" }}
-        />
+    const [isFirstLoad, setIsFirstLoad] = useState(true);
 
-        <motion.div className='fixed top-0 bottom-0 right-full w-screen h-screen z-30 bg-colors-light'
-            initial={{ x: "100%", width: "100%" }}
-            animate={{ x: "0%", width: "0%" }}
-            transition={{ delay: 0.2, duration: 0.8, ease: "easeInOut" }}
-        />
+    useEffect(() => {
+        setIsFirstLoad(false);
+    }, []);
 
-        <motion.div className='fixed top-0 bottom-0 right-full w-screen h-screen z-20 bg-colors-dark'
-            initial={{ x: "100%", width: "100%" }}
-            animate={{ x: "0%", width: "0%" }}
-            transition={{ delay: 0.4, duration: 0.8, ease: "easeInOut" }}
-        />
-    </>)
+    return (
+        <>
+            {!isFirstLoad && (
+                <>
+                    <motion.div className='fixed top-0 bottom-0 right-full w-screen h-screen z-40 bg-colors-primary'
+                        initial={{ x: "100%", width: "100%" }}
+                        animate={{ x: "0%", width: "0%" }}
+                        exit={{ x: ["0%", "100%"], width: ["0%", "100%"] }}
+                        transition={{ duration: 0.8, ease: "easeInOut" }}
+                    />
+
+                    <motion.div className='fixed top-0 bottom-0 right-full w-screen h-screen z-30 bg-colors-light'
+                        initial={{ x: "100%", width: "100%" }}
+                        animate={{ x: "0%", width: "0%" }}
+                        transition={{ delay: 0.2, duration: 0.8, ease: "easeInOut" }}
+                    />
+
+                    <motion.div className='fixed top-0 bottom-0 right-full w-screen h-screen z-20 bg-colors-dark'
+                        initial={{ x: "100%", width: "100%" }}
+                        animate={{ x: "0%", width: "0%" }}
+                        transition={{ delay: 0.4, duration: 0.8, ease: "easeInOut" }}
+                    />
+                </>
+            )}
+        </>
+    );
 }
